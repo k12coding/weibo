fd31746b2407e89a9739e6503628e19666079297
k12coding <945862485@qq.com>
2020-04-01 10:33:22 +0000

    命名路由

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-</head>
+  <head>
+    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  </head>
   <body>
     @include('layouts._header')
 
