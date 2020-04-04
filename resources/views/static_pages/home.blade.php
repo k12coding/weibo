9f5cd4ddd842ffc26b8586e0dbc0861bc7962fe8
k12coding <945862485@qq.com>
2020-04-04 15:48:23 +0000

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,16 +3,25 @@
 @section('content')
   @if (Auth::check())
     <div class="row">
+
       <div class="col-md-8">
+
         <section class="status_form">
-        @include('shared._status_form')
+          @include('shared._status_form')
         </section>
+
+        <h4>微博列表</h4>
+        <hr>
+        @include('shared._feed')
+
       </div>
+
       <aside class="col-md-4">
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
         </section>
       </aside>
+
     </div>
   @else
     <div class="jumbotron">
